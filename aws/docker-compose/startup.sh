9c445313262df6274b277f8a79f1524ebd09cde2
xcodeassociated <janusz.majchrzak@protonmail.com>
2021-05-03 16:32:52 +0200

    Update startup dev cluster script

--- aws/docker-compose/startup.sh
+++ aws/docker-compose/startup.sh
@@ -2,6 +2,27 @@
 
 is_user_root () { [ "$(id -u)" -eq 0 ]; }
 
+init_mongodb () {
+  echo "
+  use test;
+  db.test.insert({\"name\": \"test insert\"});
+  db.createUser(
+          {
+              user: \"user\",
+              pwd: \"user\",
+              roles: [
+                  {
+                      role: \"readWrite\",
+                      db: \"test\"
+                  }
+              ]
+          }
+  );
+
+  rs.initiate();
+  " | docker exec -i mongodb-cluster mongo
+}
+
 if is_user_root; then
     echo "info: Rootcheck ok"
 else
@@ -10,6 +31,9 @@ else
 fi
 
 echo "info: Starting up..."
+
+echo "info: Turning down existing cluster..."
+docker-compose -f ./docker-compose_dev.yml down
 
 docker stop $(docker ps -a -q)
 if [ $? -eq 0 ]; then
@@ -25,31 +49,27 @@ else
     echo "error: Could not remove kafka volumes"
 fi
 
+
+if pgrep -x "haproxy -f haproxy/haproxy.cfg" || pgrep -x "sudo haproxy -f haproxy/haproxy.cfg" > /dev/null
+then
+    echo "info: Haproxy is running, restarting..."
+    pkill -9 "haproxy -f haproxy/haproxy.cfg"
+    pkill -9 "sudo haproxy -f haproxy/haproxy.cfg"
+else
+    echo "info: Haproxy is not running"
+fi
+
 echo "info: Starting up haproxy..."
 haproxy -f haproxy/haproxy.cfg &
 
 echo "info: Starting up ELP dev cluster..."
 docker-compose -f ./docker-compose_dev.yml up --remove-orphans &
 
-sleep 5
+sleep 5;
 
-echo "
-db.createCollection(\"test\");
-db.createUser(
-        {
-            user: \"user\",
-            pwd: \"user\",
-            roles: [
-                {
-                    role: \"readWrite\",
-                    db: \"test\"
-                }
-            ]
-        }
-);
-db.test.insert({\"name\": \"test insert\"});
-
-rs.initiate();
-" | docker exec -i mongodb-cluster mongo
+if init_mongodb | grep -q 'WriteCommandError'; then
+   sleep 2;
+   init_mongodb
+fi
 
 echo "info: Startup finished"
